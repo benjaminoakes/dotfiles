f0d06d39497ee9013a91a03900f2d9d94dbfe538
Ben Oakes <hello@benjaminoakes.com>
2021-03-27 18:47:22 +0000

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+gem install homesick
+
+homesick clone benjaminoakes/homesick-vi-everywhere
+homesick symlink homesick-vi-everywhere --force
+homesick clone ContinuityControl/dotfiles
+homesick symlink dotfiles --force
